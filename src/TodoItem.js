e87f0d0aa79366694ebf8ca6ed1f46a6bd2f3da2
Poi9912 <jut9912@gmail.com>
2022-02-23 00:25:34 -0500

    bug no se puede tener estado doble

--- src/TodoItem.js
+++ src/TodoItem.js
@@ -1,11 +1,14 @@
 import React from "react";
 import './TodoItem.css';
 
+const iconCheck = "Icon-check--active";
+const itemCompleted = "item-content--complete";
+
 function TodoItem(props){
     return(
         <li className="item">
-            <span className={`Icon-check ${props.completed} && 'Icon-check--active'`}> &#10004; </span>
-            <p className={`item-content ${props.completed} && 'item-content--complete'`}>{props.text}</p>
+            <span className={`Icon-check ${props.completed} && ${iconCheck}`}> &#10004; </span>
+            <p className={`item-content ${props.completed} && ${itemCompleted}`}>{props.text}</p>
             <span className="Icon Icon-delete">&#10006;</span>
         </li>
     );
