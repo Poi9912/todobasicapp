feba4031215dbdd2d81ef0736b1e379312edb4ee
Poi9912 <jut9912@gmail.com>
2022-02-22 19:36:01 -0500

    añadir estilo en lineas con objetos json

--- src/TodoCounter.js
+++ src/TodoCounter.js
@@ -1,8 +1,13 @@
 import React from "react";
 
+const style = {
+    color: "red",
+    backgroundColor: "yellow"
+}
+
 function TodoCounter() {
     return(
-        <h2>Has completado 2 de 3 Tareas</h2>
+        <h2 style={style}>Has completado 2 de 3 Tareas</h2>
     );
 }
 
