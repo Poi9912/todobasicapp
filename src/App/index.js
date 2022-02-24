a35ed747d13634a671d481e438e69ae65f9f991d
Poi9912 <jut9912@gmail.com>
2022-02-24 01:57:35 -0500

    empaquetado de modulos y separación de UI de la lógica principal

--- src/App/index.js
+++ src/App/index.js
@@ -1,9 +1,5 @@
 import React from "react";
-import { TodoCounter } from "./TodoCounter";
-import { TodoSearch } from "./TodoSearch";
-import { TodoList } from "./TodoList";
-import { TodoItem } from "./TodoItem";
-import { CreateTodoButton } from "./CreateTodoButton";
+import { AppUI } from "./AppUI";
 
 const defaultTodos = [
   { text: "Cortar cebolla", completed: false },
@@ -46,32 +42,15 @@ function App() {
   }
 
   return (
-    <React.Fragment>
-      <TodoCounter 
+    <AppUI 
       total={totalTodos}
       complete={completedTodos}
-      />
-      <TodoSearch
       searchValue={searchValue}
       setSearchValue={setSearchValue}
-      />
-      <TodoList>
-        {searchedTodos.map(todo => (
-          <TodoItem  
-          key={todo.text} 
-          text={todo.text} 
-          completed={todo.completed} 
-          onComplete={()=>{
-            completeTodo(todo.text);
-          }}
-          onDelete={()=>{
-            deleteTodo(todo.text);
-          }}
-          />
-        ))}
-      </TodoList>
-      <CreateTodoButton />
-    </React.Fragment>
+      searchedTodos={searchedTodos}
+      completeTodo={completeTodo}
+      deleteTodo={deleteTodo}
+    />
   );
 }
 
