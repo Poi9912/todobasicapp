583fa34fa73e37799c64ef1bb4745a64d3b17fbb
Poi9912 <jut9912@gmail.com>
2022-02-24 01:00:43 -0500

    estado reactivo añadido en todosearch

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -1,14 +1,19 @@
 import React from "react";
 import './TodoSearch.css';
+
 function TodoSearch(){
+
+    const [searchValue, setSearchValue] = React.useState('');
 
     const onChangeValueSearch = (event) =>{
         console.log(event.target.value);
+        setSearchValue(event.target.value);
     }
 
     return(
         <input placeholder="Buscar..." className="search-input"
         onChange={onChangeValueSearch}
+        value={searchValue}
         />
     );
 }
